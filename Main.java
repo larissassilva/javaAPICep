b4f22507bfa7cecc3e5d19c5f3c1d6b6b50d3825
Larissa Soares Silva <larissa.ssilva@gmail.com>
2022-11-16 18:37:13 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+import javaViaCep.Endereco;
+import javaViaCep.ServicoDeCep;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.print("Informe seu CEP: ");
+        String cep = new Scanner(System.in).nextLine();
+        Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
+
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Localidade: " + endereco.getLocalidade());
+    }
+
+}
+
+//TODO: Autor: Larissa Soares Silva
